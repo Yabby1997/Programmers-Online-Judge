9bd89db82fbcf8b830a885b41f97fd80de0f8296
yabby <yabby1997@gmail.com>
2021-06-23 18:09:09 +0900

    43238 입국심사 C++ 풀이 리팩터

--- 43238.cpp
+++ 43238.cpp
@@ -1,30 +1,22 @@
 #include <string>
 #include <vector>
 
-#define MAX 100000000000000
-
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-    long long answer = 0;
-    
     long long from = 0;
-    long long to = MAX;
+    long long to = 100000000000000;
     long long mid = (long long)((from + to) / 2);
-    while(true) {
-        if(from == to) return from;
-        
+    
+    while(from < to) {
         long long processed = 0;
-        for(auto each : times) {
-            processed += (long long)(mid / each);
-        }
-        if(processed >= n) {
-            to = mid;
-            mid = (long long)((from + to) / 2);
-        } else {
-            from = mid + 1;
-            mid = (long long)((from + to) / 2);
-        }
+        for(auto each : times) processed += (long long)(mid / each);
+        
+        if(processed >= n) to = mid;
+        else from = mid + 1;
+        
+        mid = (long long)((from + to) / 2);
     }
-    return answer;
+    
+    return from;
 }
