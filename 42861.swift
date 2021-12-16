6d8d38c5a3bcea36acdcb8db8d818621cf858cb2
yabby <yabby1997@gmail.com>
2021-12-16 15:43:16 +0900

    42861 섬 연결하기 Swift 코드 개선

--- 42861.swift
+++ 42861.swift
@@ -1,39 +1,31 @@
 import Foundation
 
-var connection: [Int] = []
-
-func union(_ from: Int, _ to: Int) {
-    if to == connection[to] {
-        connection[to] = from
-    } else {
-        union(from, connection[to])
-    }
+func union(_ parents: inout [Int], from: Int, to: Int) {
+    let fromRoot = find(&parents, of: from)
+    let toRoot = find(&parents, of: to) 
+    parents[fromRoot] = toRoot
 }
 
-func find(_ index: Int) -> Int {
-    if index == connection[index] {
-        return index
-    } else {
-        connection[index] = find(connection[index])
-        return connection[index]
-    }
+func find(_ parents: inout [Int], of index: Int) -> Int {
+    if parents[index] == index { return index }
+    parents[index] = find(&parents, of: parents[index])
+    return parents[index]
 }
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
     var totalCost = 0
     var sortedCosts = costs.sorted { $0[2] > $1[2] }
-    
-    connection = Array(0..<n)
+    var parents = Array(0..<n)
     
     while let currentBridge = sortedCosts.popLast() {
         let from = currentBridge[0]
         let to = currentBridge[1]
         let cost = currentBridge[2]
         
-        if find(from) == find(to) {
+        if find(&parents, of: from) == find(&parents, of: to) {
             continue
         } else {
-            union(from, to)
+            union(&parents, from: from, to: to)
             totalCost += cost
         }
     }
