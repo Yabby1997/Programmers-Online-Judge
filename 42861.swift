39e29b2efce9dd947283944b6975386a79486263
yabby <yabby1997@gmail.com>
2021-12-16 15:07:40 +0900

    42861 섬 연결하기 Swift로 풀이

--- 42861.swift
+++ 42861.swift
@@ -1,48 +1,41 @@
 import Foundation
 
-struct Stack<T> {
-    private var elements: [T]
-    var isEmpty: Bool { self.elements.isEmpty }
-    
-    init(elements: [T]) {
-        self.elements = elements
+var connection: [Int] = []
+
+func union(_ from: Int, _ to: Int) {
+    if to == connection[to] {
+        connection[to] = from
+    } else {
+        union(from, connection[to])
     }
-    
-    mutating func push(_ element: T) {
-        self.elements.append(element)
-    }
-    
-    mutating func pop() -> T? {
-        return self.elements.popLast()
+}
+
+func find(_ index: Int) -> Int {
+    if index == connection[index] {
+        return index
+    } else {
+        connection[index] = find(connection[index])
+        return connection[index]
     }
 }
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
     var totalCost = 0
-    var connection = Array(repeating: 0, count: n)
-    var group = 1
-    var bridgeStack = Stack<[Int]>(elements: costs.sorted { $0[2] > $1[2] })
+    var sortedCosts = costs.sorted { $0[2] > $1[2] }
     
-    while let currentBridge = bridgeStack.pop() {
+    connection = Array(0..<n)
+    
+    while let currentBridge = sortedCosts.popLast() {
         let from = currentBridge[0]
         let to = currentBridge[1]
         let cost = currentBridge[2]
         
-        switch (connection[from], connection[to]) {
-            case (0, 0):
-            connection[from] = group
-            connection[to] = group
-            group += 1
-            case (0, 1...):
-            connection[from] = connection[to]
-            case (1..., 0):
-            connection[to] = connection[from]
-            case let (fromGroup, toGroup) where fromGroup != toGroup:
-            connection = connection.map { $0 == fromGroup ? toGroup : $0 }
-            default:
+        if find(from) == find(to) {
             continue
+        } else {
+            union(from, to)
+            totalCost += cost
         }
-        totalCost += cost 
     }
     
     return totalCost
